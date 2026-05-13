7bb094f134dfa9a7891d054f51933537e8c010ab
JoseCoca98 <cocatudelajosemanuel@gmail.com>
2026-05-13 20:56:09 +0200

    feat: prepare project for deployment

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const authRoutes = require('./routes/authRoutes')
@@ -8,7 +9,9 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 // Middlewares globales
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173'
+}))
 app.use(express.json())
 
 // Rutas
